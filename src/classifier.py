9af07f0a1bda354d290e89f62479e825aed474ae
QuentinJGMace <quentinjg.mace@gmail.com>
2024-04-04 15:51:19 +0200

    roberta sentiment analysis

--- src/classifier.py
+++ src/classifier.py
@@ -1,13 +1,16 @@
 from typing import List
+import pandas as pd
 from utils import *
 
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
 from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
+from transformers import AutoConfig, AutoTokenizer, TFAutoModelForSequenceClassification, AutoModelForSequenceClassification, AdamW
 
-
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class Classifier:
@@ -26,8 +29,15 @@ class Classifier:
         This should create and initilize the model. Does not take any arguments.
         
         """
-        self.tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
-        self.model = AutoModelForSequenceClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+        self.tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+        base_model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+        self.embedder = base_model.roberta
+        self.classifier = nn.Sequential(nn.Linear(768*2, 768), nn.GELU(), nn.Linear(768, 3))
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.softmax = nn.Softmax(dim=1)
+
+        for param in self.embedder.parameters():
+            param.requires_grad = False
     
     
     
@@ -42,54 +52,73 @@ class Classifier:
         """
         corresp_inv = {'neutral':1, 'positive':2, 'negative': 0}
         data_raw = load_file(train_filename)
+        sentences = [row[4] for row in data_raw]
+        words = [row[2] for row in data_raw]
         texts = [f'[CLS] {row[4]}  [SEP] {row[2]} [SEP]'    for row in data_raw]
         labels = [corresp_inv[row[0]] for row in data_raw]
-        texts_train, texts_val, labels_train, labels_val = train_test_split(texts, labels, test_size=0.3, random_state=42)
+
+        texts_train, texts_val, words_train, words_test, labels_train, labels_val = train_test_split(sentences, words, labels, test_size=0.3, random_state=42)
 
         
         # Tokenize input texts
         inputs_train = self.tokenizer(texts_train, padding=True, truncation=True, return_tensors='pt')
         inputs_val = self.tokenizer(texts_val, padding=True, truncation=True, return_tensors='pt')
+        # Tokenize input words
+        inputs_train_words = self.tokenizer(words_train, padding=True, truncation=True, return_tensors='pt')
+        inputs_val_words = self.tokenizer(words_test, padding=True, truncation=True, return_tensors='pt')
         # Convert labels to tensors
         labels_train = torch.tensor(labels_train)
         labels_val = torch.tensor(labels_val)
 
 
         # Create PyTorch datasets and data loaders
-        train_dataset = TensorDataset(inputs_train['input_ids'], inputs_train['attention_mask'], torch.tensor(labels_train))
-        val_dataset = TensorDataset(inputs_val['input_ids'], inputs_val['attention_mask'], torch.tensor(labels_val))
+        train_dataset = TensorDataset(inputs_train['input_ids'], inputs_train['attention_mask'], inputs_train_words["input_ids"], inputs_train_words["attention_mask"], torch.tensor(labels_train))
+        val_dataset = TensorDataset(inputs_val['input_ids'], inputs_val['attention_mask'], inputs_val_words["input_ids"], inputs_val_words["attention_mask"], torch.tensor(labels_val))
 
         train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
         val_loader = DataLoader(val_dataset, batch_size=32)
 
         # Define optimizer and training settings
-        optimizer = AdamW(self.model.parameters(), lr=5e-5)
-        self.model.to(device)
+        optimizer = AdamW(self.classifier.parameters(), lr=1e-5)
+        self.embedder.to(device)
+        self.classifier.to(device)
         # Training loop
         num_epochs = 15
+        self.embedder.train()
+
+        loss_history = []
         for epoch in range(num_epochs):
-            self.model.train()
-            for batch in train_loader:
-                input_ids, attention_mask, labels = batch
-                input_ids, attention_mask, labels = input_ids.to(device), attention_mask.to(device), labels.to(device)
+            self.classifier.train()
+            for batch in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}'):
+                input_ids, attention_mask, input_word_ids, attention_word_mask, labels = batch
+                input_ids, attention_mask, input_word_ids, attention_word_mask, labels = input_ids.to(device), attention_mask.to(device), input_word_ids.to(device), attention_word_mask.to(device), labels.to(device)
 
                 optimizer.zero_grad()
-                outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-                loss = outputs.loss
+                embeddings = self.embedder(input_ids=input_ids, attention_mask=attention_mask)["last_hidden_state"][:, 0, :]
+                embeddings_words = self.embedder(input_ids=input_word_ids, attention_mask=attention_word_mask)["last_hidden_state"][:, 0, :]
+
+                concat_embeddings = torch.cat([embeddings, embeddings_words], dim=1)
+                outputs = self.classifier(concat_embeddings)
+                loss = self.loss_fn(outputs, labels)
                 loss.backward()
+                loss_history.append(loss.item())
                 optimizer.step()
 
             # Validation
-            self.model.eval()
+            self.classifier.eval()
             total_correct = 0
             total_examples = 0
             for batch in val_loader:
-                input_ids, attention_mask, labels = batch
-                input_ids, attention_mask, labels = input_ids.to(device), attention_mask.to(device), labels.to(device)
+                input_ids, attention_mask, input_word_ids, attention_word_mask, labels = batch
+                input_ids, attention_mask, input_word_ids, attention_word_mask, labels = input_ids.to(device), attention_mask.to(device), input_word_ids.to(device), attention_word_mask.to(device), labels.to(device)
 
                 with torch.no_grad():
-                    outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
-                    logits = outputs.logits
+                    embeddings = self.embedder(input_ids=input_ids, attention_mask=attention_mask)["last_hidden_state"][:, 0, :]
+                    embeddings_words = self.embedder(input_ids=input_word_ids, attention_mask=attention_word_mask)["last_hidden_state"][:, 0, :]
+
+                    concat_embeddings = torch.cat([embeddings, embeddings_words], dim=1)
+                    outputs = self.classifier(concat_embeddings)
+                    logits = self.softmax(outputs)
                     preds = torch.argmax(logits, dim=1)
 
                 total_correct += (preds == labels).sum().item()
@@ -97,6 +126,9 @@ class Classifier:
 
             accuracy = total_correct / total_examples
             print(f'Epoch {epoch + 1}/{num_epochs}, Validation Accuracy: {accuracy:.4f}')
+
+        plt.plot(pd.Series(loss_history).rolling(100).mean())
+        plt.show()
 
 
     def predict(self, data_filename: str, device: torch.device) -> List[str]:
@@ -108,27 +140,40 @@ class Classifier:
         """
 
         test_data = load_file(data_filename)
+        sentences = [row[4] for row in test_data]
+        words = [row[2] for row in test_data]
         inputs_test = [f'[CLS] {row[4]}  [SEP] {row[2]} [SEP]'   for row in test_data]
         labels_test = [row[0]   for row in test_data]
 
         corresp = {1: 'neutral', 2: 'positive', 0:'negative'}
 
         outputs_all = []
-        for x in inputs_test:
+        self.embedder.to(device)
+        self.classifier.to(device)
+        for s,w in zip(sentences, words):
             # Input text
-            input_text = x
+            input_text = s
+            input_word = w
 
             # Tokenize input text
-            inputsi = self.tokenizer(input_text, return_tensors="pt")
+            input_s = self.tokenizer(input_text, return_tensors="pt")
+            input_words = self.tokenizer(input_word, return_tensors="pt")
 
             # Move input tensors to appropriate device
-            inputsi = {key: value.to(device) for key, value in inputsi.items()}
+            input_s = {key: value.to(device) for key, value in input_s.items()}
+            input_words = {key: value.to(device) for key, value in input_words.items()}
 
             # Perform inference
-            outputs = self.model(**inputsi)
+            with torch.no_grad():
+                embeddings = self.embedder(input_ids=input_s['input_ids'], attention_mask=input_s["attention_mask"])["last_hidden_state"][:, 0, :]
+                embeddings_words = self.embedder(input_ids=input_words['input_ids'], attention_mask=input_words["attention_mask"])["last_hidden_state"][:, 0, :]
+
+                concat_embeddings = torch.cat([embeddings, embeddings_words], dim=1)
+                outputs = self.classifier(concat_embeddings)
+                logits = self.softmax(outputs)
 
             # Get predicted label
-            predicted_class = outputs.logits.argmax().item()
+            predicted_class = torch.argmax(logits, dim=1).item()
 
             # Print predicted class
             outputs_all.append(corresp[predicted_class])
